dcf926bc74c6a063cf5dbd916afa5c173a35b1aa
ltrii <lelandtr@gmail.com>
2019-05-31 09:36:03 -0700

    Init, answers work, and most of work done (working on main read and print)

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>
 
 /**
  * Main
@@ -7,12 +9,27 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  struct direc *direc;
+  DIR *dir;
 
+  struct stat buf;
   // Open directory
-
+  if (argv[1] != NULL)
+  {
+    printf("%d is argc", argc);
+    dir = opendir(argv[1]);
+  }
+  else
+  {
+    dir = opendir(".");
+  }
+  char filepath[] = "./";
   // Repeatly read and print entries
+  while ((direc = readdir(dir)) != NULL)
+  {
 
+  }
   // Close directory
-
+  closedir(dir);
   return 0;
 }
